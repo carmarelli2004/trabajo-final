c3629088c73967d72bad9f4a113dd89e767b8df5
carmarelli2004 <131424713+carmarelli2004@users.noreply.github.com>
2023-11-23 23:48:20 -0300

    Add files via upload

---
+++ src/app/core/constant/categorias.ts
@@ -0,0 +1,48 @@
+import { Producto } from "../interfaces/producto";
+
+export const CATEGORIAS: Producto[] = [
+    {
+        nombre: 'Desayuno Tradicional',
+        id: 1,
+        precio: 15000,
+        descripcion: "Un desayuno tradicional que ofrece medialunas, limones, té, chipas, mini torta y galletitas. Un equilibrio perfecto entre sabores dulces y salados para comenzar el día con satisfacción.",
+        imagen: "https://artelpasteleria.com/wp-content/uploads/2021/09/desayuno-premium-1.jpg"
+    },
+    {
+        nombre: 'Desayuno saludable',
+        id: 2,
+        precio: 16000,
+        descripcion: "Un desayuno saludable con yogur, frutas frescas, jugo de naranja, waffles de avena y un dulce a base de frutas y frutos secos. Una forma deliciosa de comenzar tu día con energía y sabor.",
+        imagen:"https://onebiteperu.com/api/scrooge/file/FL-45B99824",
+         
+    },
+    {
+        nombre: 'Desayuno Vegano',
+        id: 3,
+        precio: 18000,
+        descripcion: "Un desayuno vegano lleno de vitalidad con cereales integrales crujientes, frutas frescas, jugos recién exprimido, te, medialunas y galletas vegana de avena. Un comienzo fresco y saludable para tu día.",
+        imagen: "https://acdn.mitiendanube.com/stores/528/608/products/ae4fa4c5-6081-44ad-a3b4-68c93d567a97_nube-e11b299ff9bff45ca016022848096939-480-0.jpg",
+    },
+    {
+        nombre: 'Desayuno celíaco',
+        id: 4,
+        precio: 17000,
+        descripcion: "Un desayuno apto para celiacos incluye alfajorcitos de maicena, barritas de cereal sin gluten, yogur, galletas sin gluten y un exprimido de naranja.Todo especialmente elaborado para celiacos.",
+        imagen: "https://www.cookies.uy/wp-content/uploads/2021/04/DSC6774-scaled.jpg",
+    },
+    {
+        nombre: 'Desayuno cumpleaños',
+        id: 5,
+        precio: 17500,
+        descripcion: "Desayuno de cumpleaños con un refrescante exprimido de naranja, tostadas de pan blanco con mermelada cacera y una porcion de torta a elección, para que el cumpleañero arranque el dia con su torta.",
+        imagen: "https://desayunospersonalizados.com/wp-content/uploads/2022/12/cumple-box-2_2.jpg",
+         
+    },
+    {
+        nombre: 'Desayuno infantil',
+        id: 6,
+        precio: 18000,
+        descripcion: "Desayuno infantil con cookies, donas, chocolates, medialunas y una chocolatada ",
+        imagen:"https://desayunospersonalizados.com/wp-content/uploads/2022/07/104.jpg",
+    },
+]
